d8701a4ba65a449e68dc6195190d45c207ad8987
Joe Powers <54547147+ReadySetJoe@users.noreply.github.com>
2025-02-04 22:37:17 -0500

    gets rid of unused backend stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ApolloProvider } from "@apollo/client";
 import { EB_Garamond } from "@next/font/google";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-
-import apolloClient from "../apollo/client";
 
 const ebGaramond = EB_Garamond({
   weight: "400",
@@ -21,12 +18,10 @@ const darkTheme = createTheme({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={ebGaramond.className}>
-      <ApolloProvider client={apolloClient}>
-        <ThemeProvider theme={darkTheme}>
-          <CssBaseline />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </ApolloProvider>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </main>
   );
 }
--- prisma/migrations/20221124082648_add_image/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "LorePage" ADD COLUMN     "image" TEXT;
