2908682bc8783a33146506b3f73fa9756421ad70
Joe Powers <54547147+ReadySetJoe@users.noreply.github.com>
2025-08-02 17:50:58 -0400

    fix for deploy 3

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Landing from "../components/screens/Landing";
+import Landing from "../components/screens/landing";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
